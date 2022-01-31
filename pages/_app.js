d6ecfa507762142a9e3f736eb3ed52af2023780c
eduardo-raniero <eduardoraniero@gmail.com>
2022-01-31 19:38:55 -0300

    banner-and-menu

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+import Menu from '../components/menu';
+
+function GlobalStyle(){
+  return(
+    <style>
+      {`
+        * {
+          color: #ccc;
+          margin: 0 auto;
+        }
+
+        body {
+          background-color: #022640;
+        }
+
+        button {
+          outline: none;
+          border: none;
+          cursor: pointer;
+          transition: 0.25s;
+        }
+      `}
+    </style>
+  )
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Menu />
+      <Component {...pageProps} />
+    </>
+    )
 }
 
 export default MyApp
