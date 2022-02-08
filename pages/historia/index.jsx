ee4fd8f9a1f1de06352a1eecd4b11bed84305a8b
eduardo-raniero <eduardoraniero@gmail.com>
2022-02-08 20:37:24 -0300

    removing-graphic

--- pages/historia/index.jsx
+++ pages/historia/index.jsx
@@ -15,11 +15,11 @@ function History(){
                     <div className={styles.textGrid}>
                         <div>
                         <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 
                             1500s, when an unknown printer took a <strong>galley of type</strong> and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
                         </p>
                         <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 
                             1500s, when an <strong>unknown printer</strong> took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
                         </p>
                         </div>
