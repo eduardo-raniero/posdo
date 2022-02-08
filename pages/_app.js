e5ebdccbdd15096ffc50ae1c9acaa7f4ad73a93e
eduardo-raniero <eduardoraniero@gmail.com>
2022-02-08 20:43:57 -0300

    font-head

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 //COMPONENTS
 import Menu from '../components/menu/menu';
 import Footer from '../components/footer/footer';
@@ -8,6 +10,13 @@ import '../styles/global.css';
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700;900&family=Playfair+Display:wght@700&display=swap'
+          rel="stylesheet"
+        />
+      </Head>
       <Menu />
       <Component {...pageProps} />
       <Footer />
